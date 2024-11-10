a11bce57306c2051f097d692cd53585bace2b7b8
YjinBuZZiLo <evgene2305@gmail.com>
2024-11-11 00:55:17 +0300

    Add files via upload

--- 10_5.py
+++ 10_5.py
@@ -1,29 +1,29 @@
-import time
-from multiprocessing import Pool
+import datetime
+import multiprocessing
+
 
 def read_info(name):
     all_data = []
-    with open(name, 'r') as file:
+    with open(name) as file:
         while True:
             line = file.readline()
+            all_data.append(line)
             if not line:
                 break
-            all_data.append(line.strip())
-    return all_data
+
+
+file_names = [f'./file {number}.txt' for number in range(1, 5)]
 
 if __name__ == "__main__":
-    filenames = [f'./file {number}.txt' for number in range(1, 5)]
 
-    # # Линейный вызов
-    start_time = time.time()
-    for filename in filenames:
-        data = read_info(filename)
-    linear_time = time.time() - start_time
-    print(f"{linear_time} (линейный)")
+    start1 = datetime.datetime.now()
+    for file_name in file_names:
+        read_info(file_name)
+    end1 = datetime.datetime.now()
+    print(f'{end1 - start1} (Линейный вызов)')
 
-    # Многопроцессный
-    start_time = time.time()
-    with Pool(processes=4) as pool:
-        result = pool.map(read_info, filenames)
-    multi_process_time = time.time() - start_time
-    print(f"{multi_process_time} (многопроцессный)")
+    with multiprocessing.Pool(processes=4) as pool:
+        start2 = datetime.datetime.now()
+        pool.map(read_info, file_names)
+        end2 = datetime.datetime.now()
+    print(f'{end2 - start2} (Многопроцессный вызов)')
